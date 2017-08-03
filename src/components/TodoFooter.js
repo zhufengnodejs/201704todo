c76a866fc674e152ead37afeb6d20d2177829427
zhufengnodejs <zhufengnodejs@126.com>
2017-08-03 17:53:28 +0800

    增加todo

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-export default class TodoFooter extends Component{
+import {connect} from 'react-redux';
+class TodoFooter extends Component{
   render(){
     return (
       <div className="row">
@@ -7,9 +8,10 @@ export default class TodoFooter extends Component{
           你还有件{this.props.activeCount}待办事项
         </div>
         <div className="col-sm-5">
-          <button className="btn btn-default">全部</button>
-          <button style={{marginLeft:5}} className="btn btn-default">未完成</button>
-          <button style={{marginLeft:5}} className="btn btn-default">已完成</button>
+          <button className={"btn btn-"+(this.props.filter=='all'?'warning':'default')}>全部</button>
+          <button style={{marginLeft:5}}
+                  className={"btn btn-"+(this.props.filter=='active'?'warning':'default')}>未完成</button>
+          <button style={{marginLeft:5}} className={"btn btn-"+(this.props.filter=='completed'?'warning':'default')}>已完成</button>
         </div>
         <div className="col-sm-3">
           <button className="btn btn-danger">删除已完成</button>
@@ -18,3 +20,11 @@ export default class TodoFooter extends Component{
     )
   }
 }
+let mapStateToProps = state => (
+  {
+    activeCount:state.todos.list.filter(item=>!item.completed).length,
+    filter:state.filter //all active completed
+  }
+)
+//let  mapDispatchToProps = dispatch =>
+export default connect(mapStateToProps)(TodoFooter);
